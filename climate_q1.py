7ae633722721e9b8e61dada1bdc26944398835d9
AndyMars8 <andes.ma@griffithuni.edu.au>
2023-09-14 14:46:30 +1000

    Code for both Python files completed

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,24 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
+# Connect to database
+connection = sqlite3.connect("climate.db")
+cursor = connection.cursor()
+
+# Fetch climate data from database
+cursor.execute("SELECT * FROM climatedata")
+items = cursor.fetchall()
+
+# Initialise variables
 years = []
 co2 = []
 temp = []
+
+# Append variables from data
+for t in items:
+    years.append(t[0])
+    co2.append(t[1])
+    temp.append(t[2])
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
--- climate_q2.py
+++ climate_q2.py
@@ -1,9 +1,19 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# Read CSV file
+df = pd.read_csv("./climate.csv")
+
+# Initialise variables
 years = []
 co2 = []
 temp = []
+
+# Append variables from data
+for i in range(len(df)):
+    years.append(df.iloc[i, 0])
+    co2.append(df.iloc[i, 1])
+    temp.append(df.iloc[i, 2])
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
